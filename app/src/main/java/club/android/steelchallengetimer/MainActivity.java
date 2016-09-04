00536f1e2869218a17b5feb7d6f318e0e3b92130
sugano_junpei <sugano_junpei@interprism.co.jp>
2016-09-04 23:36:56 +0900

    設定画面の作成

--- app/src/main/java/club/android/steelchallengetimer/MainActivity.java
+++ app/src/main/java/club/android/steelchallengetimer/MainActivity.java
@@ -1,10 +1,11 @@
 package club.android.steelchallengetimer;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.media.AudioManager;
 import android.media.ToneGenerator;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,16 +17,17 @@ import android.media.MediaRecorder;
 import java.text.SimpleDateFormat;
 
 
-public class MainActivity extends AppCompatActivity implements Runnable, View.OnClickListener {
+public class MainActivity extends Activity implements Runnable, View.OnClickListener {
 
     private long startTime;
     // 10 msec order
     private int period = 10;
     private int derayTime = 2000;
     private int SAMPLE_RATE = 44100;
+    public static final int CALL_RESULT_CODE = 100;
     private double baseValue;
     private TextView timerText;
-    private Button startButton, stopButton, resetButton;
+    private Button startButton, stopButton, configButton;
 
     private Thread thread = null;
     private final Handler handler = new Handler();
@@ -44,31 +46,30 @@ public class MainActivity extends AppCompatActivity implements Runnable, View.On
         timerText.setText(dataFormat.format(0));
         startButton = (Button) findViewById(R.id.start_button);
         stopButton = (Button) findViewById(R.id.stop_button);
-        resetButton = (Button) findViewById(R.id.reset_button);
-
+        configButton = (Button) findViewById(R.id.config_button);
 
         startButton.setOnClickListener(this);
         // @todo あとで消す
         stopButton.setOnClickListener(this);
-        resetButton.setOnClickListener(this);
+        configButton.setOnClickListener(this);
         stopButton.setEnabled(false);
-        resetButton.setEnabled(false);
+        configButton.setEnabled(false);
         bufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE,
                 AudioFormat.CHANNEL_IN_MONO,
                 AudioFormat.ENCODING_PCM_16BIT);
         audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC,
                 SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO,
                 AudioFormat.ENCODING_PCM_16BIT, bufferSize);
-        baseValue = 12.0;
+        baseValue = 15.0;
         audioRecord.stop();
     }
 
     @Override
     public void onClick(View v) {
-        if (v == startButton) {
+        if (v.getId() == R.id.start_button) {
             startButton.setEnabled(false);
             stopButton.setEnabled(true);
-            resetButton.setEnabled(true);
+            configButton.setEnabled(true);
             stopRun = false;
             audioRecord.startRecording();
             thread = new Thread(this);
@@ -79,21 +80,22 @@ public class MainActivity extends AppCompatActivity implements Runnable, View.On
                 stopRun = true;
             }
             ToneGenerator toneGenerator
-                    = new ToneGenerator(AudioManager.STREAM_SYSTEM, ToneGenerator.MAX_VOLUME);
-            toneGenerator.startTone(ToneGenerator.TONE_PROP_BEEP);
+                    = new ToneGenerator(AudioManager.STREAM_ALARM, ToneGenerator.MAX_VOLUME);
+            toneGenerator.startTone(ToneGenerator.TONE_CDMA_ABBR_ALERT);
             thread.start();
             startTime = System.currentTimeMillis();
-        } else if (v == stopButton) {
+        } else if (v.getId() == R.id.stop_button) {
             startButton.setEnabled(false);
             stopButton.setEnabled(false);
-            resetButton.setEnabled(true);
+            configButton.setEnabled(true);
             stopRun = true;
             thread = null;
-        } else {
-            resetButton.setEnabled(false);
-            stopButton.setEnabled(false);
-            startButton.setEnabled(true);
-            timerText.setText(dataFormat.format(0));
+        } else if (v.getId() == R.id.config_button){
+            // 設定画面の呼び出し用
+            Intent intent = new Intent(this, SubActivity.class);
+            intent.putExtra("baseValue", baseValue);
+            // startActivityで起動する
+            startActivityForResult(intent, CALL_RESULT_CODE);
         }
     }
 
